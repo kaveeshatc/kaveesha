376446c29181e5b70a2d5ac3094fe77cd9c256bb
kaveeshatc <kaveeshatc@gmail.com>
2021-12-19 10:49:43 +0530

    history

--- src/routes/paduvasdeva.svelte
+++ src/routes/paduvasdeva.svelte
@@ -1,2 +1,10 @@
 <h2>King paduvasdeva</h2>
 <p>*According to the Mahavansa chronicle, Prince Paduvasdeva 504 BC – 474 BC Was king of time.<br/> මහාවංශ වංශකතාවට අනුව, පඬුවස්දේව රජු (ක්‍රි.පූ. 504 – 474) කාලයේ රජු විය.</p>
+<br/>
+<p>*His farther's name is sumiththa. <br/>  ඔහුගේ පියාගේ නම සුමිත්තයි.  </p>
+<br/>
+<p>*His mother's name is madha . <br/>  ඔහුගේ මවගේ නම මාදායි.  </p>
+<br/>
+<p>
+    *Prince Panduvasudeva, was King of Upatissa Nuwara  from 504 BC to 474 BC. He was the first monarch of the Kingdom of Upatissa Nuwara succeeding Upatissa, who reigned as regent. Panduvasudeva had ten sons, including Abhaya, Tissa and his only daughter Unmada Chitra. 
+    <br/> ක්‍රිපූ 504 සිට ක්‍රිපූ 474 දක්වා උපතිස්ස නුවර රජ වූයේ පඬුවාසුදේව කුමරුය. ඔහු උපතිස්ස රජතුමාගෙන් පසුව රජ වූ උපතිස්ස නුවර රාජධානියේ පළමු රජ විය. පඬුවාසුදේවයන්ට අභය, තිස්ස සහ ඔහුගේ එකම දියණිය උන්මාදා චිත්‍රා ඇතුළු පුතුන් දස දෙනෙක් සිටියහ. </p>
--- src/routes/vijaya.svelte
+++ src/routes/vijaya.svelte
@@ -1,6 +1,8 @@
 <h2>King Vijaya</h2>
 <p>*According to the Mahavansa chronicle, Prince Vijaya was the first Sinhalese king.<br/> මහාවංශ වංශකතාවට අනුව, විජය කුමරු (ක්‍රි.පූ. 543-505) පළමු සිංහල රජු විය.</p>
 <br/>
+<p>*Thambapanni was the place where King Vijaya set foot in Ceylon. <br/>  විජය රජු ලංකාවට පය තැබූ ස්තානය තම්බපන්නියයි.  </p>
+<br/>
 <p>*His farther's name is sinhabahu. <br/>  ඔහුගේ පියාගේ නම සිංහබාහු ය.  </p>
 <br/>
 <p>*His mother's name is sinhasivali. <br/>  ඔහුගේ මවගේ නම සිංහසීවලී.  </p>
@@ -19,6 +21,31 @@
 <br/>
 <p> Concerned in old age that he would die without an heir, he decided to bring his twin brother Sumitta from India to govern his kingdom. Vijaya sent a letter to Sumitta, but died before receiving a reply. His ministers from Upatissagāma then governed the kingdom for a year while they waited for a reply. In Sinhapura, Sumitta had become king and had three sons. His queen was a daughter of the king of Madda (possibly Madra). When Vijaya's messengers arrived, Sumitta asked one of his sons to go to Lanka because he was too old; Panduvasdeva, his youngest son, volunteered. Panduvasdeva and 32 sons of Sumitta's ministers reached Lanka, where he became the new ruler. <br/> තමාට උරුමක්කාරයෙකු නොමැතිව මිය යනු ඇතැයි මහලු වියේදී කනස්සල්ලට පත් වූ ඔහු තම රාජධානිය පාලනය කිරීමට තම නිවුන් සොහොයුරා වන සුමිත්ත ඉන්දියාවෙන් ගෙන්වා ගැනීමට තීරණය කළේය. විජය සුමිත්තට ලිපියක් යැවූ නමුත් පිළිතුරක් ලැබීමට පෙර මිය ගියේය. උපතිස්සගම සිට ඔහුගේ ඇමතිවරු පිළිතුරක් බලාපොරොත්තුවෙන් සිටියදී වසරක් රාජ්‍යය පාලනය කළහ. සිංහපුරයේදී සුමිත්ත රජ වී පුතුන් තිදෙනෙකු සිටියේය. ඔහුගේ බිසව මද්දා රජුගේ දියණියකි. විජයගේ දූතයන් පැමිණි විට සුමිත්ත තම පුතෙකුට වයස වැඩි නිසා ලංකාවට යන ලෙස ඉල්ලා සිටියේය. ඔහුගේ බාල පුත් පඬුවස්දේව ස්වේච්ඡාවෙන් ඉදිරිපත් විය. පඬුවස්දේව සහ සුමිත්තගේ අමාත්‍යවරුන්ගේ පුතුන් 32 දෙනා ලංකාවට පැමිණි අතර එහිදී ඔහු නව පාලකයා බවට පත් විය.
 </p>
+<br/> 
+<br/>
+<br/> 
+<br/>
+<img src="vi1.jpg"/>
+<br/>
+<h5>*King Vijaya's visit to Ceylon and Kuveni spinning cotton. <br/> විජය රජු ලංකාවට පැමිනීම සහ කුවේනිය කපු කටිමින් සිටින අවස්තාව. </h5>
+<br/>
+<br/> 
+<br/>
+<img src="vi2.jpg"/>
+<br/>
+<h5>*Thambapanni. <br/> තම්බපන්නිය. </h5>
+<br/>
+<br/> 
+<br/>
+<img src="vi3.jpg"/>
+<br/>
+<h5>*Kuveniya, Deesala and Jeevahatta. <br/> කුවේණිය , දීසාල සහ ජීවහත්ත. </h5>
+<br/>
+<br/> 
+<br/>
+<img src="vi4.jpg"/>
+<br/>
+<h5>*The moment Kuveni is killed. <br/>කුවේණිය මරා දමන අවස්තාව. </h5>
 <br/> 
 <br/>
 <a href="Kings"> <input type="button" value="Back Page"> </a>
